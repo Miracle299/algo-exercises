8e244c96852f35952c95895a44ddd8d73c53392c
null <vchabanenko219@gmail.com>
2021-01-03 12:30:47 +0200

    Save Commit

---
+++ src/leetcode/RotateArray.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void rotate(int[] nums, int k) {
+        int N = nums.length;
+        revert(nums, 0, N - 1);
+        revert(nums, 0, k - 1);
+        revert(nums, k, N - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void revert(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[end];
+            arr[end] = arr[start];
+            arr[start] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        rotate(new int[] {1,2,3,4,5,6,7}, 3);
+//        rotate(new int[] {-1,-100,3,99}, 2);
+    }
+}
