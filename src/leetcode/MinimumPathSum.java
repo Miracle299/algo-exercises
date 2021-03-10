423e8a7aea7df0a1b6ec66ac50fe66d43e0eb904
null <vchabanenko219@gmail.com>
2021-03-10 23:47:35 +0200

    Save Commit

--- src/leetcode/MinimumPathSum.java
+++ src/leetcode/MinimumPathSum.java
@@ -1,12 +1,42 @@
 package leetcode;
 
+import java.util.Arrays;
+
 public class MinimumPathSum {
-    //todo: complete this task
-    public int minPathSum(int[][] grid) {
-        //https://leetcode.com/problems/minimum-path-sum/
+    public static int minPathSum(int[][] grid) {
         int[][] map = new int[grid.length][grid[0].length];
+        map[0][0] = grid[0][0];
 
+        for (int i = 0; i < grid.length - 1; i++) {
+            map[0][i + 1] = map[0][i] + grid[0][i];
+        }
 
-        return 0;
+        for (int i = 1; i < grid[0].length; i++) {
+            map[i][0] += grid[i][0];
+        }
+
+//        printArr(grid);
+
+        printArr(map);
+
+        for (int i = 1; i < grid.length; i++) {
+            for (int j = 1; j < grid[0].length; j++) {
+                map[i][j] = Math.min(map[i - 1][j] + grid[i][j], map[i][j - 1] + grid[i][j]);
+                printArr(map);
+            }
+        }
+
+        return map[grid.length - 1][grid[0].length - 1];
+    }
+
+    private static void printArr(int[][] map) {
+        System.out.println("");
+        for (int[] arr : map) {
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    public static void main(String[] args) {
+        minPathSum(new int[][] { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } });
     }
 }
